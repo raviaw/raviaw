56423cfd5ff420f99f127c46a82dfc3cf37dc138
raviaw <raviaw@d194eb45-6469-8450-7960-5ffb456db75c>
2010-03-20 08:58:29 +0000

    Fixed Heapsort, added Quicksort

--- trunk/introductionToAlgorithms/src/ArrayUtils.java
+++ trunk/introductionToAlgorithms/src/ArrayUtils.java
@@ -3,11 +3,36 @@ import java.util.Random;
 public class ArrayUtils {
     final static Random r = new Random();
 
+    public static void exchange(int[] A, int i, int j) {
+        System.out.println("exchange(i = " + i + ", j = " + j + ", A[i] = " + (i < A.length ? A[i] : -1) + ", A[j] = "
+                + (j < A.length ? A[j] : -1) + ")");
+        int a = A[i];
+        A[i] = A[j];
+        A[j] = a;
+    }
+
+    public static int[] shuffledArray(final int elements, final int lowerBound) {
+        int[] A = new int[elements];
+
+        // Build
+        for (int i = 0; i < elements; i++) {
+            A[i] = i + lowerBound;
+        }
+
+        // Shuffle
+        for (int i = elements - 1; i > 0; i--) {
+            int n = r.nextInt(i);
+            exchange(A, i, n);
+        }
+
+        return A;
+    }
+
     public static int[] randomArray(int elements, int lowerBound, int higherBound) {
         int[] A = new int[elements];
         for (int i = 0; i < elements; i++)
             A[i] = r.nextInt(higherBound - lowerBound) + lowerBound;
-        
+
         return A;
     }
 
