c4465c2529aa646c05c73d14d275ca9d5ac102ee
raviaw <raviaw@d194eb45-6469-8450-7960-5ffb456db75c>
2010-03-20 07:42:22 +0000

    Initial version

---
+++ trunk/introductionToAlgorithms/src/Algorithms.java
@@ -0,0 +1,101 @@
+
+public class Algorithms {
+    private static int recursiveBreak = 0;
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        int[] elements = ArrayUtils.randomArray(30, 0, 60);
+
+        mergeSort(elements);
+        int number = 5;
+        int pos = binarySearch(elements, number, 0, elements.length - 1);
+        System.out.println("number: " + number + ", pos: " + pos + ", proof: " + (pos != -1 ? elements[pos] : -1));
+    }
+
+    public static int binarySearch(int[] A, int number, int p, int r) {
+        if (recursiveBreak++ > 100) {
+            System.out.println("HALT!");
+            return -1;
+        }
+        int mid = (p + r) / 2;
+        ArrayUtils.printArrayElements("binarySearch, number: " + number + ", p: " + p + ", r: " + r + ", mid: " + mid
+                + ", A[mid]: " + A[mid] + ", A: ", A);
+        if (A[mid] == number) {
+            return mid; // Found!
+        } else if (r - p <= 1) {
+            System.out.println("down to two elements");
+            if (A[r] == number)
+                return r;
+            else if (A[p] == number)
+                return p;
+            else
+                return -1;
+        } else if (A[mid] < number) { // number is to the right side
+            return binarySearch(A, number, mid, r);
+        } else {
+            return binarySearch(A, number, p, mid);
+        }
+    }
+
+    public static void mergeSort(int[] A) {
+        mergeSort(A, 0, A.length - 1);
+    }
+
+    public static void mergeSort(int[] A, int p, int r) {
+        ArrayUtils.printArrayElements("mergeSort, A: ", A);
+        System.out.println("mergeSort, p: " + p + ", r: " + r);
+        if (p < r) {
+            int q = (p + r) / 2;
+            mergeSort(A, p, q);
+            mergeSort(A, q + 1, r);
+            merge(A, p, q, r);
+        }
+    }
+
+    public static void merge(int[] A, int p, int q, int r) {
+        ArrayUtils.printArrayElements("before: ", A);
+
+        int n1 = q - p + 1;
+        int n2 = r - q;
+        int L[] = new int[n1 + 1];
+        int R[] = new int[n2 + 1];
+        for (int i = 0; i < n1; i++)
+            L[i] = A[p + i];
+        for (int i = 0; i < n2; i++)
+            R[i] = A[q + i + 1];
+        L[n1] = Integer.MAX_VALUE;
+        R[n2] = Integer.MAX_VALUE;
+        ArrayUtils.printArrayElements("L: ", L);
+        ArrayUtils.printArrayElements("R: ", R);
+        int i = 0;
+        int j = 0;
+        for (int k = p; k <= r; k++) {
+            if (L[i] <= R[j]) {
+                A[k] = L[i];
+                i++;
+            } else {
+                A[k] = R[j];
+                j++;
+            }
+        }
+        ArrayUtils.printArrayElements("merged: ", A);
+    }
+
+    public static void insertionSort(int[] A) {
+        ArrayUtils.printArrayElements("start: ", A);
+        for (int j = 1; j < A.length; j++) {
+            int key = A[j];
+            int i = j - 1;
+            while (i >= 0 && A[i] > key) {
+                A[i + 1] = A[i];
+                i = i - 1;
+                ArrayUtils.printArrayElements("i: " + i, A);
+            }
+            A[i + 1] = key;
+            ArrayUtils.printArrayElements("key: " + key, A);
+        }
+    }
+
+}
---
+++ trunk/introductionToAlgorithms/src/MaximumSubArray.java
@@ -0,0 +1,74 @@
+public class MaximumSubArray {
+    public static void main(String[] args) {
+        int A[] = { 13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15 - 5, 7 };
+        Result res = findMaxSubArray("", A, 0, A.length - 1);
+        ArrayUtils.printArrayElements("all:", A);
+        ArrayUtils.printArrayElements("sub (" + res + "): ", A, res.left, res.right + 1);
+    }
+
+    public static Result findMaxSubArray(String chain, int[] A, int low, int high) {
+        System.out.println("findMaxSubArray([" + chain + "]" + ", low: " + low + ", high: " + high + ")");
+        final String newChain = chain + "<low = " + low + ", high = " + high + ">";
+        if (low == high)
+            return new Result(low, high, A[low]);
+        else {
+            int mid = (low + high) / 2;
+            Result left = findMaxSubArray(newChain, A, low, mid);
+            Result right = findMaxSubArray(newChain, A, mid >= high ? high : mid + 1, high);
+            Result cross = findMaxCrossingSubArray(A, low, mid, high);
+            if (left.sum >= right.sum && left.sum >= cross.sum) {
+                return left;
+            } else if (right.sum >= left.sum && right.sum >= cross.sum) {
+                return right;
+            } else {
+                return cross;
+            }
+        }
+    }
+
+    public static Result findMaxCrossingSubArray(int[] A, int low, int mid, int high) {
+        int sum;
+
+        sum = 0;
+        int leftSum = Integer.MIN_VALUE;
+        int maxLeft = 0;
+        for (int i = mid; i >= low; i--) {
+            sum = sum + A[i];
+            if (sum > leftSum) {
+                leftSum = sum;
+                maxLeft = i;
+            }
+        }
+
+        sum = 0;
+        int rightSum = Integer.MIN_VALUE;
+        int maxRight = 0;
+        for (int i = mid + 1; i <= high; i++) {
+            sum = sum + A[i];
+            if (sum > rightSum) {
+                rightSum = sum;
+                maxRight = i;
+            }
+        }
+        Result res = new Result(maxLeft, maxRight, leftSum + rightSum);
+        System.out.println("findMaxCrossingSubArray(low: " + low + ", mid: " + mid + ", high: " + high + ") = " + res);
+        return res;
+    }
+
+    public static class Result {
+        private final int left, right, sum;
+
+        public Result(int left, int right, int sum) {
+            super();
+            this.left = left;
+            this.right = right;
+            this.sum = sum;
+        }
+
+        @Override
+        public String toString() {
+            return "Result [left=" + left + ", right=" + right + ", sum=" + sum + "]";
+        }
+
+    }
+}
